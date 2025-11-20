cd052fc093cb369728b97aecca49a613dee39ef1
Lillyions <lhdn.pje24@uea.edu.br>
2025-11-19 21:59:30 -0400

    Initial Setup

--- Assets/Scripts/InitialSetup.cs
+++ Assets/Scripts/InitialSetup.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class InitialSetup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    [SerializeField] private float requiredArea;
+    [SerializeField] private ARPlaneManager planeManager;
+    [SerializeField] private GameObject startExperienceUI;
+
+    void OnEnable()
     {
-        
+        planeManager.planesChanged += OnPlanesUpdetad;
+    }
+
+    void OnDisable()
+    {
+        planeManager.planesChanged -= OnPlanesUpdetad;
     }
 
     // Update is called once per frame
-    void Update()
+    /*void Update()
     {
-        
+        foreach (var plane in planeManager.trackables)
+        {
+            if (plane.size.x * plane.size.y >= requiredArea)
+            {
+                //Plano detectado
+                startExperienceUI.SetActive(true);
+                return;
+            }
+        }
+        //Nenhum plano detectado
+        startExperienceUI.SetActive(false);
+    }*/
+
+    public void OnClickStartExperience()
+    {
+        Debug.Log("EIniciando a experiência AR...");
+        startExperienceUI.SetActive(false);
+        planeManager.enabled = false;
+
+        foreach (var plane in planeManager.trackables)
+        {
+            plane.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnPlanesUpdetad(ARPlanesChangedEventArgs args)
+    {
+        foreach (var plane in planeManager.trackables)
+        {
+            if (plane.size.x * plane.size.y >= requiredArea)
+            {
+                //Plano detectado
+                startExperienceUI.SetActive(true);
+            }
+        }
     }
 }
